1eeecc2c4262841e1dd3b6d5fbafa97524ab3b7d
vaibhav <vaibhavsahni2001@gmail.com>
2023-07-18 22:07:39 +0530

    logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     console.log(user);
   return (
     <div className='h-24 w-full bg-gradient-to-r from-sky-500 to-indigo-500 px-5 flex justify-between items-center' style={{borderBottomLeftRadius:"5px", borderBottomRightRadius:"5px"}}>
-        <img src="https://unbounce.com/wp-content/themes/unbounce2019/assets/img/unbounce-icon-dark.svg?v=1" className='h-[40px] w-[150px]'/>
+        <img src="/logo.png" className='h-[40px] w-[150px]'/>
         <div className='relative'>
             <img src={user?.picture} onClick={() => setShowDrop(!showDrop)} className='h-10 w-10 rounded-full cursor-pointer'/>
             {showDrop && <div className='h-24 w-24 bg-black text-white font-medium absolute right-[1rem] top-[2.5rem] rounded-md pt-2 flex flex-col items-center'>
